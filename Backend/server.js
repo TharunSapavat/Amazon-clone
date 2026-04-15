a8142677704f733953515a8c71305847a73efb9b
Tharun S <sapavattharun06@gmail.com>
2026-04-15 22:56:49 +0530

    starting production

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 const allowedOrigins = [
     process.env.FRONTEND_URL,
-    'http://localhost:5173', 
+    'http://localhost:5173',
     'http://localhost:3000'
 ].filter(Boolean);
 
